d45801368d50a4b79a9365d8ef1dc161039da120
HimavanthReddyPallapu <113073979+HimavanthReddyPallapu@users.noreply.github.com>
2022-09-12 12:44:34 -0400

    Update Sample.java

--- Sample.java
+++ Sample.java
@@ -1,7 +1,47 @@
-// Time Complexity :
-// Space Complexity :
-// Did this code successfully run on Leetcode :
-// Any problem you faced while coding this :
+// Time Complexity : constant
+// Space Complexity : constant
+// Did this code successfully run on Leetcode : yes
+// Any problem you faced while coding this : no
 
 
 // Your code here along with comments explaining your approach
+
+
+import java.lang.Math;
+class MyHashMap {
+    int size=(int)Math.pow(10,6)+1;
+    int hm[];
+
+    public MyHashMap() {
+        hm=new int[size];
+        for(int i=0; i<size;i++)
+        {
+            hm[i]=-1;
+        }
+    }
+    
+    public void put(int key, int value) {
+        
+      hm[key]=value;
+        
+        
+    }
+    
+    public int get(int key) {
+        return hm[key];
+        
+    }
+    
+    public void remove(int key) {
+        
+        hm[key]=-1;
+    }
+}
+
+/**
+ * Your MyHashMap object will be instantiated and called as such:
+ * MyHashMap obj = new MyHashMap();
+ * obj.put(key,value);
+ * int param_2 = obj.get(key);
+ * obj.remove(key);
+ */
